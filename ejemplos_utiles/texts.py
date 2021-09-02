70612e760ed5f8bb595b8b9847a42ebb51437f64
AlterCodex <fjb.pacho@gmail.com>
2021-09-02 08:43:40 -0500

    <add examples>

---
+++ ejemplos_utiles/texts.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == '__main__':
+
+    print(f'int {sys.getsizeof(4)} Bytes')
+
+    print(f'string {sys.getsizeof("4")} Bytes')
+
+    print(f'float {sys.getsizeof(5.5)} Bytes')
+
+    print(f'list {sys.getsizeof([])} Bytes')
+
+
+
