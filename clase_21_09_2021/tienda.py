438511957bbbd6762193de9e5497211a790ad695
AlterCodex <fjb.pacho@gmail.com>
2021-09-19 21:04:56 -0500

    <feat> add objects part 2

---
+++ clase_21_09_2021/tienda.py
@@ -0,0 +1,15 @@
+from clase_21_09_2021.guitarra import Guitarra
+
+
+if __name__== '__main__':
+    color = str(input('ingrese el color:'))
+    marca = str(input('ingrese la marca:'))
+    madera_mastil = str(input('ingrese la madera del mastil:'))
+    guitarra = Guitarra()
+    print(guitarra)
+    
+
+
+
+
+
