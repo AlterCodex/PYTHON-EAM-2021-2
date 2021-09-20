60872e4eb1cb4fca287c9600e1cf23eedd580c84
AlterCodex <fjb.pacho@gmail.com>
2021-09-19 20:48:33 -0500

    <Inicando Objetos>

---
+++ Clase 21-09-2021/guitarra.py
@@ -0,0 +1,9 @@
+
+class Guitarra:
+
+    def __init__(self):
+        self.color='sin color'
+
+
+    def pintar(self, nuevo_color):
+        self.color = nuevo_color
---
+++ Clase 21-09-2021/objetos.py
@@ -0,0 +1,12 @@
+
+class Perro():
+
+
+
+
+
+    def acelerar(self):
+        pass
+
+    def frenar(self):
+        pass
