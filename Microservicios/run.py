9e3432513a689f3e62da2ef63f374d60e322440e
AlterCodex <fjb.pacho@gmail.com>
2021-10-28 07:54:34 -0500

    <feat endpint>

---
+++ Microservicios/run.py
@@ -0,0 +1,30 @@
+import json
+import random
+
+import falcon
+from falcon import API
+
+from Clases.clase_28_09_2021.Infraestructura.PersistenciaGuitarra import \
+    PersistenciaGuitarra
+
+
+class HolaMundo():
+
+    def on_get(self, req, resp, uuid):
+        db=PersistenciaGuitarra()
+        gui=db.load_json(uuid+'.json')
+        mensajes = ['Hola Mindo','Hola Que hace', 'Adio', 'Ciao','2+2=4']
+        resp.body = json.dumps(gui)
+        resp.status =falcon.HTTP_OK
+
+
+def iniciar() ->  API:
+
+    api = API()
+    api.add_route("/guitarra/{uuid}",HolaMundo())
+
+    return api
+
+app=iniciar()
+
+
